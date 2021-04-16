9335b22afc6307c3b22a137595565c3371a8f895
Ania Bogacka <anka.bogacka@hotmail.com>
2021-04-16 14:53:31 +0200

    add comments to navbar test

--- cypress/integration/e2e_tests/navbar.spec.js
+++ cypress/integration/e2e_tests/navbar.spec.js
@@ -5,16 +5,17 @@ describe('Navbar Test', () => {
 
   it('should display online banking content', () => {
     cy.get('#onlineBankingMenu').click()
-    cy.contains('Pay bills easily')
+    cy.contains('Pay bills easily') //cy.url().should('include', 'online-banking.html')
+    cy.get('h1').should('be.visible')
   })
 
   it('should display feedback content', () => {
     cy.get('#feedback').click()
-    cy.contains('Send Message')
+    cy.contains('Send Message') //cy.url().should('include', 'feedback.html')
   })
 
   it('should display home page content', () => {
     cy.get('.container > a').click()
-    cy.contains('Welcome to Zero Online Banking.')
+    cy.contains('Welcome to Zero Online Banking.') //cy.url().should('include', 'index.html')
   })
 })
